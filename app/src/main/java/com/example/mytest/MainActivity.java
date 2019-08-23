ef9d09ff3e01d89a7702b6cf9f64d01616b87a0f
Yrw <yangrongwei@chinatelecom.cn>
2019-08-23 09:04:45 +0800

    Toast

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Toast
         Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
     }
 }
