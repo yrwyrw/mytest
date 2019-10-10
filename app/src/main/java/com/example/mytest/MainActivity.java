fe846b7c9b5a546a1ecabac51fd4d93feece06bb
Yrw <yangrongwei@chinatelecom.cn>
2019-10-10 10:01:25 +0800

    3

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //3
         //2
         //1
         //Hello
